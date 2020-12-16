ca65b4404d5b3870b52dbf9fd84f713d5a7e241b
devlev1980 <devlev1980@gmail.com>
2020-12-16 12:05:00 +0200

    add project-2

--- src/app/components/words/words.component.ts
+++ src/app/components/words/words.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
-import {Observable, Subscription} from 'rxjs';
+import {Subscription} from 'rxjs';
 import {WordModel} from '../../models/Word.model';
 import {LocalStorageService} from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-words',
   templateUrl: './words.component.html',
-  styleUrls: ['./words.component.scss']
+  styleUrls: ['./words.component.scss'],
 })
 export class WordsComponent implements OnInit, OnDestroy {
   duckWords: WordModel[] = [];
@@ -18,7 +18,9 @@ export class WordsComponent implements OnInit, OnDestroy {
   tags: string[] = [];
 
 
-  constructor(private apiService: ApiService, private localStorageService: LocalStorageService) {
+  constructor(private apiService: ApiService,
+              private localStorageService: LocalStorageService,
+            ) {
   }
 
   ngOnDestroy(): void {
